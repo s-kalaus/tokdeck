c67063f0977a1802c19678502949b033914081cc
s-kalaus <sergey.kalaus@gmail.com>
2019-03-12 12:34:39 +0100

    ci-3

--- bin/build.sh
+++ bin/build.sh
@@ -5,3 +5,35 @@ BUILD_ID=${2:-1}
 BUILD_ID_BASE="0"
 VERSION="${BUILD_ENV}-v${BUILD_ID_BASE}.${BUILD_ID}"
 
+#frontend
+cd frontend
+npm i
+npm run ${BUILD_ENV}
+cd ..
+
+#copy release
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan -t rsa kalaus.ru > ~/.ssh/known_hosts
+cp ./ssh/id_rsa ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+mkdir .release
+cd .release
+git init
+git remote add origin ssh://git@kalaus.ru:2222/kalaus/tokdeck-release.git
+git config core.autocrlf true
+git config user.name "gitlab"
+git config user.email "sergey.kalaus+gitlab@gmail.com"
+#git fetch origin ${BUILD_ENV}
+#git checkout ${BUILD_ENV}
+git checkout -b ${BUILD_ENV}
+git rm -rf .
+git clean -fxd
+cd ..
+cp -R bin config docker express graphql lib model node_modules service ssl view Makefile package.json public .release
+cd .release
+git add .
+git commit -am ${VERSION}
+git tag ${VERSION}
+git push origin ${BUILD_ENV}
+git push origin ${VERSION}
